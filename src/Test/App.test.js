681b0a9c8ebf3b261da66714d5dc22a8d1a24426
Alejandra Martinez <martinezmayra605@gmail.com>
2022-03-15 20:07:23 -0500

    Adicion de archivos para testeo con react y Jest+Mock

---
+++ src/Test/App.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+import * as app from "./app";
+import * as math from "./math";
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it("adds correctly", ()=>{
+  expect(1+1).toEqual(2)
+})
+
+
+
+math.add = jest.fn();
+math.subtract = jest.fn();
+
+test("calls math.add", () => {
+  app.doAdd(1, 2);
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+});
+
+test("calls math.subtract", () => {
+  app.doSubtract(1, 2);
+  expect(math.subtract).toHaveBeenCalledWith(1, 2);
+});
